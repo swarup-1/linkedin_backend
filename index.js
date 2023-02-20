12ed104415b8a034fce17d2d55d24870a7c6bb1f
swarup-1 <swarupkadoli9@gmail.com>
2023-02-20 17:46:35 +0530

    Backend Created

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app=express()
 app.use(cors())
 app.use(express.json())
 app.use("/users",users)
-// app.use(authFun)
+app.use(authFun)
 app.use("/posts",posts)
 
 app.listen(process.env.port, async()=>{
